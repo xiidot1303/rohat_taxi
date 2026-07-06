57b0b19843f8091f19a6a8c21b9ea07a6d7b832a
xiidot1303 <xii1303@inbox.ru>
2026-07-06 18:20:49 +0500

    Refactor ChequeSerializer to simplify validation and save logic

--- app/serializers.py
+++ app/serializers.py
@@ -11,44 +11,28 @@ class ChequeSerializer(serializers.ModelSerializer):
         exclude = ['datetime']
 
     def get_validators(self):
-        # Remove unique validators on id field entirely
         return []
 
     def validate_id(self, value):
-        # For updates (status_code != 1), id must exist in DB
-        status_code = int(self.initial_data.get('status_code'))
-        if status_code != 1:
-            if not Cheque.objects.filter(pk=value).exists():
-                raise serializers.ValidationError("Object with this id does not exist.")
         return value
 
     def validate(self, data):
-        status_code = int(data.get('status_code'))
-        if status_code != 1 and not data.get('id'):
-            raise serializers.ValidationError({"id": "This field is required for updates."})
         return data
 
-    def is_valid(self, raise_exception=False):
-        valid = super().is_valid(raise_exception=raise_exception)
-        if not valid and self.errors.get('id') and 'This field is required for updates.' in self.errors['id']:
-            # If the error is about 'id' being required, we can ignore it for creation
-            self._errors.pop('id')
-            return True
-        return valid
-
     def save(self, **kwargs):
-        status_code = int(self.validated_data.get('status_code'))
-        if status_code == 1:
-            # CREATE
-            return Cheque.objects.create(**self.validated_data)
-        else:
-            # UPDATE — only non-blank fields
-            id = self.validated_data.pop('id')
-            instance = Cheque.objects.get(id=id)
+        cheque_id = self.validated_data.get('id')
 
-            for field, value in self.validated_data.items():
-                if value not in [None, '', [], {}]:  # skip blank values
-                    setattr(instance, field, value)
+        if cheque_id is not None:
+            instance = Cheque.objects.filter(pk=cheque_id).first()
+            if instance is not None:
+                for field, value in self.validated_data.items():
+                    if field == 'id':
+                        continue
+                    if value not in [None, '', [], {}]:
+                        setattr(instance, field, value)
 
-            instance.save()
-            return instance
+                instance.save()
+                return instance
+
+        create_data = self.validated_data.copy()
+        return Cheque.objects.create(**create_data)
